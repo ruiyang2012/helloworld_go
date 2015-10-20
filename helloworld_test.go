6352a995625a0b196332206bcc5edfdba195926a
Rui Yang <rui@citruslane.com>
2015-10-20 12:49:51 -0700

    add more test.

--- helloworld_test.go
+++ helloworld_test.go
@@ -6,37 +6,39 @@ package hello_test
 import (
 	"github.com/ruiyang2012/helloworld_go"
 
-	"io/ioutil"
-
-	"net/http"
+	"appengine/aetest"
 	"net/http/httptest"
-
 	"testing"
-
 
 	"github.com/stretchr/testify/assert"
 )
 
 var router = hello.SetupRoutes()
 
-func testRequest(t *testing.T, url string) {
-	resp, err := http.Get(url)
-	defer resp.Body.Close()
-	assert.NoError(t, err)
-
-	body, ioerr := ioutil.ReadAll(resp.Body)
-	assert.NoError(t, ioerr)
-	assert.Equal(t, "it worked", string(body), "resp body should match")
-	assert.Equal(t, "200 OK", resp.Status, "should get a 200")
-}
-
-
 func TestRunEmptyWithEnv(t *testing.T) {
-	
-	req, _ := http.NewRequest("GET", "/ping", nil)
+	inst, e := aetest.NewInstance(nil)
+	assert.NoError(t, e)
+	defer inst.Close()
+	req, e := inst.NewRequest("GET", "/ping", nil)
+	assert.NoError(t, e)
 	resp := httptest.NewRecorder()
 	router.ServeHTTP(resp, req)
 
 	assert.Equal(t, resp.Body.String(), "pong")	
+	
+}
+
+func TestRunV1Count(t *testing.T) {
+	inst, e := aetest.NewInstance(nil)
+	assert.NoError(t, e)
+	defer inst.Close()
+	_, e0 := inst.NewRequest("DELETE", "/v1/count", nil)
+	assert.NoError(t, e0)
+	req, e := inst.NewRequest("GET", "/v1/count", nil)
+	assert.NoError(t, e)
+	resp := httptest.NewRecorder()
+	router.ServeHTTP(resp, req)
+
+	assert.Equal(t, resp.Body.String(), "0")	
 	
 }
